e935fa844053f2690b5f951d171df7cff3ca3738
Rolf Würdemann <Rolf.Wuerdemann@fmf.uni-freiburg.de>
2011-11-15 12:33:35 +0100

    Fix for comment char (;/#)

--- readfmf/readfmf.py
+++ readfmf/readfmf.py
@@ -76,13 +76,23 @@ def preParseData(b):
             return match.group(0)
         return u""
     d = re.sub(dataExpr, preParseData, d)
-    return preParsedData, d, str(localVar['fmf-version'])
+    return preParsedData, d, str(localVar['fmf-version']), commentChar
 
 def stream2config(stream):
     fmfstring = stream.read()
-    preParsedData, d, FMFversion = preParseData(fmfstring)
+    preParsedData, d, FMFversion, commentChar = preParseData(fmfstring)
+    if commentChar == "#":
+        commentChar = "\#"
     from configobj import ConfigObj,ConfigObjError
     class FMFConfigObj(ConfigObj):
+        #   ConfigObj sets the following default
+        #   in opposition to FMF
+        #   Function            ConfigObj   FMF
+        #   Key-Value-Seperator "="         ":"
+        #   CommentChar         "#"         "#",";", ....
+        #
+        #   So we have to redefine our regexp
+        #
         _keyword = re.compile(r'''^ # line start
             (\s*)                   # indentation
             (                       # keyword
@@ -94,6 +104,83 @@ def stream2config(stream):
             (.*)                    # value (including list values and comments)
             $   # line end
             ''', re.VERBOSE)  #'
+
+        _sectionmarker = re.compile(r'''^
+            (\s*)                     # 1: indentation
+            ((?:\[\s*)+)              # 2: section marker open
+            (                         # 3: section name open
+                (?:"\s*\S.*?\s*")|    # at least one non-space with double quotes
+                (?:'\s*\S.*?\s*')|    # at least one non-space with single quotes
+                (?:[^'"\s].*?)        # at least one non-space unquoted
+            )                         # section name close
+            ((?:\s*\])+)              # 4: section marker close
+            \s*(%s.*)?                # 5: optional comment
+            $''' % commentChar,
+            re.VERBOSE)
+
+        _valueexp = re.compile(r'''^
+            (?:
+                (?:
+                    (
+                        (?:
+                            (?:
+                                (?:".*?")|              # double quotes
+                                (?:'.*?')|              # single quotes
+                                (?:[^'",%s][^,%s]*?)    # unquoted
+                            )
+                            \s*,\s*                     # comma
+                        )*      # match all list items ending in a comma (if any)
+                    )
+                    (
+                        (?:".*?")|                      # double quotes
+                        (?:'.*?')|                      # single quotes
+                        (?:[^'",%s\s][^,]*?)|  # unquoted
+                        (?:(?<!,))                      # Empty value
+                    )?          # last item in a list - or string value
+                )|
+                (,)             # alternatively a single comma - empty list
+            )
+            \s*(%s.*)?          # optional comment
+            $''' % ((commentChar, )*4),
+            re.VERBOSE)
+    
+        # use findall to get the members of a list value
+        _listvalueexp = re.compile(r'''
+            (
+                (?:".*?")|          # double quotes
+                (?:'.*?')|          # single quotes
+                (?:[^'",%s]?.*?)       # unquoted
+            )
+            \s*,\s*                 # comma
+            ''' % commentChar,
+            re.VERBOSE)
+    
+        # this regexp is used for the value
+        # when lists are switched off
+        _nolistvalue = re.compile(r'''^
+            (
+                (?:".*?")|          # double quotes
+                (?:'.*?')|          # single quotes
+                (?:[^'"%s].*?)|     # unquoted
+                (?:)                # Empty value
+            )
+            \s*(%s.*)?              # optional comment
+            $''' % (commentChar, commentChar),
+            re.VERBOSE)
+    
+        # regexes for finding triple quoted values on one line
+        _single_line_single = re.compile(
+                                r"^'''(.*?)'''\s*(%s.*)?$" % commentChar, 
+                                re.VERBOSE)
+        _single_line_double = re.compile(
+                                r'^"""(.*?)"""\s*(%s.*)?$' % commentChar, 
+                                re.VERBOSE)
+        _multi_line_single = re.compile(
+                                r"^(.*?)'''\s*(%s.*)?$" % commentChar, 
+                                re.VERBOSE)
+        _multi_line_double = re.compile(
+                                r'^(.*?)"""\s*(%s.*)?$' % commentChar, 
+                                re.VERBOSE)
     try:
         config = FMFConfigObj(d.encode('utf-8').splitlines(), encoding='utf-8')
     except ConfigObjError,e:
